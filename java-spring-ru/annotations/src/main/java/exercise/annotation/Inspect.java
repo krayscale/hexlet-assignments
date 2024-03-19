31e9063229f82b94ea0b14decc508bfd9adf615d
@hexlet/cli <support@hexlet.io>
2024-03-19 15:06:27 +0300

    submit java-spring-ru/annotations

--- java-spring-ru/annotations/src/main/java/exercise/annotation/Inspect.java
+++ java-spring-ru/annotations/src/main/java/exercise/annotation/Inspect.java
@@ -1,5 +1,11 @@
 package exercise.annotation;
 
-public @interface Inspect {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Inspect {
 }
