d63784d3e05c4fa041be2483293dad3e6efe5354
@hexlet/cli <support@hexlet.io>
2024-03-20 14:17:56 +0300

    submit java-spring-ru/http-crud

--- java-spring-ru/http-crud/src/main/java/exercise/Application.java
+++ java-spring-ru/http-crud/src/main/java/exercise/Application.java
@@ -19,40 +19,39 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-    @GetMapping("/pages")
-    public List<Post> getPosts(@RequestParam(defaultValue = "1") Integer page, @RequestParam(defaultValue = "10") Integer limit){
+    @GetMapping("/posts")
+    public List<Post> getPosts(
+            @RequestParam(defaultValue = "1") Integer page,
+            @RequestParam(defaultValue = "10") Integer limit) {
         return posts.stream().skip(page).limit(limit).toList();
     }
 
-    @GetMapping("/pages/{id}")
-    public Optional<Post> getPost(@PathVariable String id){
+    @GetMapping("/posts/{id}")
+    public Optional<Post> getPost(@PathVariable String id) {
         return posts.stream().filter(p -> Objects.equals(p.getId(), id)).findFirst();
     }
 
-    @PostMapping("/pages")
-    public Post createPost(@RequestBody Post post){
+    @PostMapping("/posts")
+    public Post createPost(@RequestBody Post post) {
         posts.add(post);
         return post;
     }
 
-    @PutMapping("/pages/{id}")
-    public Post updatePost(@PathVariable String id, @RequestBody Post post){
-
+    @PutMapping("/posts/{id}")
+    public Post updatePost(@PathVariable String id, @RequestBody Post post) {
         Optional<Post> mPost = posts.stream().filter(p -> Objects.equals(p.getId(), id)).findFirst();
-
-        if(mPost.isPresent()) {
+        if (mPost.isPresent()) {
             Post data = mPost.get();
             data.setId(post.getId());
             data.setBody(post.getBody());
             data.setTitle(post.getTitle());
             return data;
         }
-
         return null;
     }
 
-    @DeleteMapping("/pages/{id}")
-    public void deletePort(@PathVariable String id){
+    @DeleteMapping("/posts/{id}")
+    public void deletePort(@PathVariable String id) {
         posts.removeIf(p -> Objects.equals(p.getId(), id));
     }
 }
